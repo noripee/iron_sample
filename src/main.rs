92b887b63570ce48631432569336da03b2bcfb10
air <air@air-no-MacBook-Air.local>
2015-10-27 03:31:44 +0900

    add router sample

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 extern crate iron;
+extern crate router;
 
 use iron::prelude::*;
 use iron::status;
+use router::Router;
 
 fn main() {
-    fn hello_world(_: &mut Request) -> IronResult<Response> {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }
+  let mut router = Router::new();
 
-    Iron::new(hello_world).http("localhost:3000").unwrap();
-    println!("On 3000");
+  router.get("/", hello_world);
+  router.get("/random", random);
+  router.post("/random", random_post);
+
+  fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello World!")))
+  }
+
+  fn random(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Make Random Dial Happen Here!")))
+  }
+
+  fn random_post(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Make Random Dial Happen Here!")))
+  }
+
+  Iron::new(router).http("localhost:3000").unwrap();
+  println!("On 3000");
 }
-
